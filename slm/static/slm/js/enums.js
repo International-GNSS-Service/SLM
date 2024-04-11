729623dd4c3912ffac7c8dc928d675226f37d223
Brian Kohan <bkohan@gmail.com>
2024-04-11 10:25:44 -0700

    limit dependencies

--- slm/static/slm/js/enums.js
+++ slm/static/slm/js/enums.js
@@ -25,74 +25,28 @@ class LogEntryType {
 		this.css = css;
 	}
 	
+	static ciCompare(a, b) {
+		return typeof a === 'string' && typeof b === 'string' ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0 : a === b;
+	}
+	
 	toString() {
 		return this.label;
 	}
 	
 	static get(value) {
-		switch(value) {
-			case 1:
-				return LogEntryType.SITE_PROPOSED;
-			case 2:
-				return LogEntryType.ADD;
-			case 3:
-				return LogEntryType.UPDATE;
-			case 4:
-				return LogEntryType.DELETE;
-			case 5:
-				return LogEntryType.PUBLISH;
-			case 6:
-				return LogEntryType.LOG_UPLOAD;
-			case 7:
-				return LogEntryType.IMAGE_UPLOAD;
-			case 8:
-				return LogEntryType.ATTACHMENT_UPLOAD;
-			case 9:
-				return LogEntryType.IMAGE_PUBLISH;
-			case 10:
-				return LogEntryType.ATTACHMENT_PUBLISH;
-			case 11:
-				return LogEntryType.IMAGE_UNPUBLISH;
-			case 12:
-				return LogEntryType.ATTACHMENT_UNPUBLISH;
-			case 13:
-				return LogEntryType.IMAGE_DELETE;
-			case 14:
-				return LogEntryType.ATTACHMENT_DELETE;
-			case 15:
-				return LogEntryType.REVERT;
+		if (value instanceof this) {
+			return value;
 		}
-		switch(value) {
-			case "SITE_PROPOSED":
-				return LogEntryType.SITE_PROPOSED;
-			case "ADD":
-				return LogEntryType.ADD;
-			case "UPDATE":
-				return LogEntryType.UPDATE;
-			case "DELETE":
-				return LogEntryType.DELETE;
-			case "PUBLISH":
-				return LogEntryType.PUBLISH;
-			case "LOG_UPLOAD":
-				return LogEntryType.LOG_UPLOAD;
-			case "IMAGE_UPLOAD":
-				return LogEntryType.IMAGE_UPLOAD;
-			case "ATTACHMENT_UPLOAD":
-				return LogEntryType.ATTACHMENT_UPLOAD;
-			case "IMAGE_PUBLISH":
-				return LogEntryType.IMAGE_PUBLISH;
-			case "ATTACHMENT_PUBLISH":
-				return LogEntryType.ATTACHMENT_PUBLISH;
-			case "IMAGE_UNPUBLISH":
-				return LogEntryType.IMAGE_UNPUBLISH;
-			case "ATTACHMENT_UNPUBLISH":
-				return LogEntryType.ATTACHMENT_UNPUBLISH;
-			case "IMAGE_DELETE":
-				return LogEntryType.IMAGE_DELETE;
-			case "ATTACHMENT_DELETE":
-				return LogEntryType.ATTACHMENT_DELETE;
-			case "REVERT":
-				return LogEntryType.REVERT;
+		
+		for (const en of this) {
+			if (en.value === value) {
+				return en;
+			}
+		}
+		for (const en of this) {
+			if (this.ciCompare(en.name, value)) {
+				return en;
+			}
 		}
 		return null;
 	}
@@ -103,20 +57,24 @@ class LogEntryType {
 }
 class SiteLogStatus {
 	
-	static FORMER = new SiteLogStatus(1, "FORMER", "Former", "slm-status-former", "#3D4543", "Site is no longer maintained and logs are not published.");
-	static PROPOSED = new SiteLogStatus(2, "PROPOSED", "Proposed", "slm-status-proposed", "#913D88", "This is a new Site that has never been published.");
-	static UPDATED = new SiteLogStatus(3, "UPDATED", "Updated", "slm-status-updated", "#0079AD", "Site log or section has unpublished updates.");
-	static PUBLISHED = new SiteLogStatus(4, "PUBLISHED", "Published", "slm-status-published", "#0D820D", "Site log or section is published with no unpublished changes.");
-	static EMPTY = new SiteLogStatus(5, "EMPTY", "Empty", "slm-status-empty", "#D3D3D3", "Site log section is empty or deleted.");
-	static SUSPENDED = new SiteLogStatus(6, "SUSPENDED", "Suspended", "slm-status-suspended", "#E0041A", "Site has been temporarily suspended and does not appear in public data.");
+	static FORMER = new SiteLogStatus(1, "FORMER", "Former", "Site is no longer maintained and logs are not published.", "slm-status-former", "#3D4543");
+	static PROPOSED = new SiteLogStatus(2, "PROPOSED", "Proposed", "This is a new Site that has never been published.", "slm-status-proposed", "#913D88");
+	static UPDATED = new SiteLogStatus(3, "UPDATED", "Updated", "Site log or section has unpublished updates.", "slm-status-updated", "#0079AD");
+	static PUBLISHED = new SiteLogStatus(4, "PUBLISHED", "Published", "Site log or section is published with no unpublished changes.", "slm-status-published", "#0D820D");
+	static EMPTY = new SiteLogStatus(5, "EMPTY", "Empty", "Site log section is empty or deleted.", "slm-status-empty", "#D3D3D3");
+	static SUSPENDED = new SiteLogStatus(6, "SUSPENDED", "Suspended", "Site has been temporarily suspended and does not appear in public data.", "slm-status-suspended", "#E0041A");
 	
-	constructor (value, name, label, css, color, help) {
+	constructor (value, name, label, help, css, color) {
 		this.value = value;
 		this.name = name;
 		this.label = label;
+		this.help = help;
 		this.css = css;
 		this.color = color;
-		this.help = help;
+	}
+	
+	static ciCompare(a, b) {
+		return typeof a === 'string' && typeof b === 'string' ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0 : a === b;
 	}
 	
 	toString() {
@@ -124,33 +82,19 @@ class SiteLogStatus {
 	}
 	
 	static get(value) {
-		switch(value) {
-			case 1:
-				return SiteLogStatus.FORMER;
-			case 2:
-				return SiteLogStatus.PROPOSED;
-			case 3:
-				return SiteLogStatus.UPDATED;
-			case 4:
-				return SiteLogStatus.PUBLISHED;
-			case 5:
-				return SiteLogStatus.EMPTY;
-			case 6:
-				return SiteLogStatus.SUSPENDED;
+		if (value instanceof this) {
+			return value;
 		}
-		switch(value) {
-			case "FORMER":
-				return SiteLogStatus.FORMER;
-			case "PROPOSED":
-				return SiteLogStatus.PROPOSED;
-			case "UPDATED":
-				return SiteLogStatus.UPDATED;
-			case "PUBLISHED":
-				return SiteLogStatus.PUBLISHED;
-			case "EMPTY":
-				return SiteLogStatus.EMPTY;
-			case "SUSPENDED":
-				return SiteLogStatus.SUSPENDED;
+		
+		for (const en of this) {
+			if (en.value === value) {
+				return en;
+			}
+		}
+		for (const en of this) {
+			if (this.ciCompare(en.name, value)) {
+				return en;
+			}
 		}
 		return null;
 	}
@@ -173,34 +117,33 @@ class AlertLevel {
 		this.color = color;
 	}
 	
+	static ciCompare(a, b) {
+		return typeof a === 'string' && typeof b === 'string' ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0 : a === b;
+	}
+	
 	toString() {
 		return this.name;
 	}
 	
 	static get(value) {
-		switch(value) {
-			case 1:
-				return AlertLevel.NOTICE;
-			case 2:
-				return AlertLevel.WARNING;
-			case 3:
-				return AlertLevel.ERROR;
+		if (value instanceof this) {
+			return value;
 		}
-		switch(value) {
-			case "NOTICE":
-				return AlertLevel.NOTICE;
-			case "WARNING":
-				return AlertLevel.WARNING;
-			case "ERROR":
-				return AlertLevel.ERROR;
+		
+		for (const en of this) {
+			if (en.value === value) {
+				return en;
+			}
 		}
-		switch(value) {
-			case "NOTICE":
-				return AlertLevel.NOTICE;
-			case "WARNING":
-				return AlertLevel.WARNING;
-			case "ERROR":
-				return AlertLevel.ERROR;
+		for (const en of this) {
+			if (this.ciCompare(en.name, value)) {
+				return en;
+			}
+		}
+		for (const en of this) {
+			if (en.label === value) {
+				return en;
+			}
 		}
 		return null;
 	}
@@ -211,19 +154,23 @@ class AlertLevel {
 }
 class SiteFileUploadStatus {
 	
-	static UNPUBLISHED = new SiteFileUploadStatus(1, "UNPUBLISHED", "Unpublished File", "slm-upload-unpublished file", "#0079AD", "The file is pending moderation before it will be made public.");
-	static PUBLISHED = new SiteFileUploadStatus(2, "PUBLISHED", "Published File", "slm-upload-published file", "#0D820D", "The file is published and is publicly available as an attachment to the site.");
-	static INVALID = new SiteFileUploadStatus(3, "INVALID", "Invalid Site Log", "slm-upload-invalid site log", "#8b0000", "The file did not pass validation.");
-	static WARNINGS = new SiteFileUploadStatus(4, "WARNINGS", "Warnings Site Log", "slm-upload-warnings site log", "#E3AA00", "The file is valid but has some warnings.");
-	static VALID = new SiteFileUploadStatus(5, "VALID", "Valid Site Log", "slm-upload-valid site log", "#0D820D", "The file is valid.");
+	static UNPUBLISHED = new SiteFileUploadStatus(1, "UNPUBLISHED", "Unpublished File", "The file is pending moderation before it will be made public.", "slm-upload-unpublished file", "#0079AD");
+	static PUBLISHED = new SiteFileUploadStatus(2, "PUBLISHED", "Published File", "The file is published and is publicly available as an attachment to the site.", "slm-upload-published file", "#0D820D");
+	static INVALID = new SiteFileUploadStatus(3, "INVALID", "Invalid Site Log", "The file did not pass validation.", "slm-upload-invalid site log", "#8b0000");
+	static WARNINGS = new SiteFileUploadStatus(4, "WARNINGS", "Warnings Site Log", "The file is valid but has some warnings.", "slm-upload-warnings site log", "#E3AA00");
+	static VALID = new SiteFileUploadStatus(5, "VALID", "Valid Site Log", "The file is valid.", "slm-upload-valid site log", "#0D820D");
 	
-	constructor (value, name, label, css, color, help) {
+	constructor (value, name, label, help, css, color) {
 		this.value = value;
 		this.name = name;
 		this.label = label;
+		this.help = help;
 		this.css = css;
 		this.color = color;
-		this.help = help;
+	}
+	
+	static ciCompare(a, b) {
+		return typeof a === 'string' && typeof b === 'string' ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0 : a === b;
 	}
 	
 	toString() {
@@ -231,29 +178,19 @@ class SiteFileUploadStatus {
 	}
 	
 	static get(value) {
-		switch(value) {
-			case 1:
-				return SiteFileUploadStatus.UNPUBLISHED;
-			case 2:
-				return SiteFileUploadStatus.PUBLISHED;
-			case 3:
-				return SiteFileUploadStatus.INVALID;
-			case 4:
-				return SiteFileUploadStatus.WARNINGS;
-			case 5:
-				return SiteFileUploadStatus.VALID;
+		if (value instanceof this) {
+			return value;
 		}
-		switch(value) {
-			case "UNPUBLISHED":
-				return SiteFileUploadStatus.UNPUBLISHED;
-			case "PUBLISHED":
-				return SiteFileUploadStatus.PUBLISHED;
-			case "INVALID":
-				return SiteFileUploadStatus.INVALID;
-			case "WARNINGS":
-				return SiteFileUploadStatus.WARNINGS;
-			case "VALID":
-				return SiteFileUploadStatus.VALID;
+		
+		for (const en of this) {
+			if (en.value === value) {
+				return en;
+			}
+		}
+		for (const en of this) {
+			if (this.ciCompare(en.name, value)) {
+				return en;
+			}
 		}
 		return null;
 	}
@@ -278,42 +215,38 @@ class SiteLogFormat {
 		this.alts = alts;
 	}
 	
+	static ciCompare(a, b) {
+		return typeof a === 'string' && typeof b === 'string' ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0 : a === b;
+	}
+	
 	toString() {
 		return this.label;
 	}
 	
 	static get(value) {
-		switch(value) {
-			case 1:
-				return SiteLogFormat.LEGACY;
-			case 2:
-				return SiteLogFormat.GEODESY_ML;
-			case 3:
-				return SiteLogFormat.JSON;
+		if (value instanceof this) {
+			return value;
 		}
-		switch(value) {
-			case "LEGACY":
-				return SiteLogFormat.LEGACY;
-			case "GEODESY_ML":
-				return SiteLogFormat.GEODESY_ML;
-			case "JSON":
-				return SiteLogFormat.JSON;
+		
+		for (const en of this) {
+			if (en.value === value) {
+				return en;
+			}
 		}
-		switch(value) {
-			case "text/plain":
-				return SiteLogFormat.LEGACY;
-			case "application/xml":
-				return SiteLogFormat.GEODESY_ML;
-			case "application/json":
-				return SiteLogFormat.JSON;
+		for (const en of this) {
+			if (this.ciCompare(en.name, value)) {
+				return en;
+			}
 		}
-		switch(value) {
-			case "log":
-				return SiteLogFormat.LEGACY;
-			case "xml":
-				return SiteLogFormat.GEODESY_ML;
-			case "json":
-				return SiteLogFormat.JSON;
+		for (const en of this) {
+			if (this.ciCompare(en.mimetype, value)) {
+				return en;
+			}
+		}
+		for (const en of this) {
+			if (this.ciCompare(en.ext, value)) {
+				return en;
+			}
 		}
 		return null;
 	}
@@ -335,34 +268,33 @@ class SLMFileType {
 		this.type = type;
 	}
 	
+	static ciCompare(a, b) {
+		return typeof a === 'string' && typeof b === 'string' ? a.localeCompare(b, undefined, { sensitivity: 'accent' }) === 0 : a === b;
+	}
+	
 	toString() {
 		return this.label;
 	}
 	
 	static get(value) {
-		switch(value) {
-			case 1:
-				return SLMFileType.SITE_LOG;
-			case 2:
-				return SLMFileType.SITE_IMAGE;
-			case 3:
-				return SLMFileType.ATTACHMENT;
+		if (value instanceof this) {
+			return value;
 		}
-		switch(value) {
-			case "SITE_LOG":
-				return SLMFileType.SITE_LOG;
-			case "SITE_IMAGE":
-				return SLMFileType.SITE_IMAGE;
-			case "ATTACHMENT":
-				return SLMFileType.ATTACHMENT;
+		
+		for (const en of this) {
+			if (en.value === value) {
+				return en;
+			}
 		}
-		switch(value) {
-			case "log":
-				return SLMFileType.SITE_LOG;
-			case "image":
-				return SLMFileType.SITE_IMAGE;
-			case "attachment":
-				return SLMFileType.ATTACHMENT;
+		for (const en of this) {
+			if (this.ciCompare(en.name, value)) {
+				return en;
+			}
+		}
+		for (const en of this) {
+			if (en.type === value) {
+				return en;
+			}
 		}
 		return null;
 	}
